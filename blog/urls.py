234994c18491bf613e1c075119190c70bb65c1eb
Carol Gitonga <=>
2023-03-29 13:05:02 +0300

    send emails

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,10 @@ from . import views
 app_name = 'blog'#allows to organize URLs by application and use the name when referring to them
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
 
     #use the publication date and slug for the post detail URL
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail')
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+
+    path('<int:post_id>/share/', views.post_share, name='post_share')
 ]
